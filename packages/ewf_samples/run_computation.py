c314a58cfafa73c1436164d7937b381c95c210fd
MehmetErenTuranboy <mehmet.turanboy@gmail.com>
2023-06-27 18:39:13 +0200

    feat: add python script for illustration

--- packages/ewf_samples/run_computation.py
+++ packages/ewf_samples/run_computation.py
@@ -1,7 +1,9 @@
 import subprocess
 
 # Execute the TypeScript file using ts-node
-result = subprocess.run(['ts-node', '../origin-247-claim/tests/computation.ts'], capture_output=True, text=True)
+result_claim = subprocess.run(['ts-node', '../origin-247-claim/tests/computation.ts'], capture_output=True, text=True)
+result_transfer = subprocess.run(['ts-node', '../origin-247-transfer/test/computation.ts'], capture_output=True, text=True)
 
 # Print the output
-print(result.stdout)
+print(result_claim.stdout)
+print(result_transfer.stdout)
